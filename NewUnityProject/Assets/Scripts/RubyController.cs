331cf762f6dbb10ab5c22b162893dd7c64c075af
Jian Jang <pureunnabi@gmail.com>
2023-04-07 14:24:45 +0900

    Character move
    Character move

--- NewUnityProject/Assets/Scripts/RubyController.cs
+++ NewUnityProject/Assets/Scripts/RubyController.cs
@@ -13,10 +13,12 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()   
     {
-         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x += -0.01f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
